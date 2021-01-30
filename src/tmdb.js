7734f19dd712dfc56e46f3b3fbffa08b84db0c34
Brian McMillen <brianmcmillen1@gmail.com>
2021-01-30 20:36:20 +0000

    logs

--- src/tmdb.js
+++ src/tmdb.js
@@ -1,5 +1,7 @@
 const KEY = process.env.TMDB_API_KEY || import.meta.env.VITE_TMDB_API_KEY;
 
+console.log('tmdb', { KEY });
+
 export async function getNicCage() {
   const resp = await fetch(`https://api.themoviedb.org/3/person/2963?api_key=${KEY}&language=en-US`)
   const data = await resp.json();
