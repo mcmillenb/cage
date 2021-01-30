ee48a3e3fa8546544ca668adad7f37f2be596578
Brian McMillen <brianmcmillen1@gmail.com>
2021-01-30 20:15:51 +0000

    try process.env

--- src/tmdb.js
+++ src/tmdb.js
@@ -1,4 +1,4 @@
-const KEY = import.meta.env.VITE_TMDB_API_KEY;
+const KEY = process.env.TMDB_API_KEY || import.meta.env.VITE_TMDB_API_KEY;
 
 export async function getNicCage() {
   const resp = await fetch(`https://api.themoviedb.org/3/person/2963?api_key=${KEY}&language=en-US`)
